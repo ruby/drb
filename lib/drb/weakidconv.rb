b360d746f023c6955ee43563077df3c5f023b508
Sutou Kouhei <kou@clear-code.com>
2025-04-28 17:07:56 +0900

    Avoid use of id2ref for weak mapping (#35)
    [Bugs #15711]
    
    https://bugs.ruby-lang.org/issues/15711
    
    The implementation of `ObjectSpace._id2ref` on JRuby (and TruffleRuby)
    is very expensive, and on JRuby we normally do not have it enabled
    because it impacts performance of the entire runtime.
    
    This uses `ObjectSpace::WeakMap` instead of `ObjectSpace._id2ref` by
    default and deprecates `DRb::WeakIdConv`.
    
    Authored-by: Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>

--- lib/drb/weakidconv.rb
+++ lib/drb/weakidconv.rb
@@ -1,57 +1,29 @@
 # frozen_string_literal: false
 require_relative 'drb'
-require 'monitor'
 
 module DRb
 
+  # DRb::WeakIdConv is deprecated since 2.2.1. You don't need to use
+  # DRb::WeakIdConv instead of DRb::DRbIdConv. It's the same class.
+  #
+  # This file still exists for backward compatibility.
+  #
   # To use WeakIdConv:
   #
   #  DRb.start_service(nil, nil, {:idconv => DRb::WeakIdConv.new})
 
-  class WeakIdConv < DRbIdConv
-    class WeakSet
-      include MonitorMixin
-      def initialize
-        super()
-        @immutable = {}
-        @map = ObjectSpace::WeakMap.new
-      end
-
-      def add(obj)
-        synchronize do
-          begin
-            @map[obj] = self
-          rescue ArgumentError
-            @immutable[obj.__id__] = obj
-          end
-          return obj.__id__
-        end
-      end
-
-      def fetch(ref)
-        synchronize do
-          @immutable.fetch(ref) {
-            @map.each { |key, _|
-              return key if key.__id__ == ref
-            }
-            raise RangeError.new("invalid reference")
-          }
-        end
-      end
-    end
-
-    def initialize()
-      super()
-      @weak_set = WeakSet.new
-    end
-
-    def to_obj(ref) # :nodoc:
-      return super if ref.nil?
-      @weak_set.fetch(ref)
-    end
-
-    def to_id(obj) # :nodoc:
-      return @weak_set.add(obj)
+  def self.const_missing(name) # :nodoc:
+    case name
+    when :WeakIdConv
+      warn("DRb::WeakIdConv is deprecated. " +
+           "You can use the DRb::DRbIdConv. " +
+           "You don't need to use this.",
+           uplevel: 1)
+      const_set(:WeakIdConv, DRbIdConv)
+      singleton_class.remove_method(:const_missing)
+      DRbIdConv
+    else
+      super
     end
   end
 end
