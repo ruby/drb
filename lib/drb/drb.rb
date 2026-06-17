af312ee0488c21f333e4525bea19d505417d1290
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2026-06-17 09:17:09 +0900

    Remove dangling reference to the dRuby home page from the overview
    The "(see References)" note pointed at a section that does not exist in
    the README, and the claim that the full package must be downloaded
    separately is obsolete now that drb ships as a gem.
    
    https://github.com/ruby/drb/issues/32

--- lib/drb/drb.rb
+++ lib/drb/drb.rb
@@ -17,12 +17,6 @@
 # Ruby process to invoke methods on an object in another Ruby process on the
 # same or a different machine.
 #
-# The Ruby standard library contains the core classes of the dRuby package.
-# However, the full package also includes access control lists and the
-# Rinda tuple-space distributed task management system, as well as a
-# large number of samples.  The full dRuby package can be downloaded from
-# the dRuby home page (see *References*).
-#
 # For an introduction and examples of usage see the documentation to the
 # DRb module.
 #
